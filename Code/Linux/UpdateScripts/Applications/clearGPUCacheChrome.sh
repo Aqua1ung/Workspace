1aca43a8b7b9e53b510acae4e7abab5c3e84ac3b
Cristian Cocos <cristi@ieee.org>
2023-09-21 12:22:57 -0400

    Clear ALL GPUCaches.

--- Code/Linux/UpdateScripts/Applications/clearGPUCacheChrome.sh
+++ Code/Linux/UpdateScripts/Applications/clearGPUCacheChrome.sh
@@ -9,7 +9,7 @@ fi
 
 echo "Clearing Chrome's GPUCache ..."
 printf '\n' # Insert blank line.
-find /home/"$USER"/.config/google-chrome -type d -name GPUCache | while read path
+find /home/"$USER"/.config -type d -name GPUCache | while read path
 do
 rm "$path"/*
 done
