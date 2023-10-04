abfb779f5aaf31c2de274ef37124e16b22ddae75
Cristian Cocos <cristi@ieee.org>
2023-10-04 18:46:48 -0400

    Fixed AppIndicator.

---
+++ Code/Linux/fixAppIndicator57.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Do NOT run as root!
+if [ $(id -u) == 0 ]
+then
+  echo "This script should NOT be run as root! Exiting ..."
+  exit
+fi
+
+gsettings set org.gnome.shell disable-user-extensions true
+
+cd /home/$USER/Downloads
+wget -N https://github.com/ubuntu/gnome-shell-extension-appindicator/archive/refs/tags/v57.tar.gz
+tar -xf /home/$USER/Downloads/v57.tar.gz
+cd /home/$USER/Downloads/gnome-shell-extension-appindicator-57
+rm -rf indicator-test-tool lint locale schemas .github
+cd -
+rm -rf /home/$USER/.local/share/gnome-shell/extensions/appindicatorsupport@rgcjonas.gmail.com/interfaces-xml
+mv -f /home/$USER/Downloads/gnome-shell-extension-appindicator-57/* /home/$USER/.local/share/gnome-shell/extensions/appindicatorsupport@rgcjonas.gmail.com
+# mv -f /home/$USER/Downloads/gnome-shell-extension-appindicator-57/interfaces-xml /home/$USER/.local/share/gnome-shell/extensions/appindicatorsupport@rgcjonas.gmail.com
+# mv -f /home/$USER/Downloads/gnome-shell-extension-appindicator-57/schemas /home/$USER/.local/share/gnome-shell/extensions/appindicatorsupport@rgcjonas.gmail.com
+
+gsettings set org.gnome.shell disable-user-extensions false
+
+rm -rf *
