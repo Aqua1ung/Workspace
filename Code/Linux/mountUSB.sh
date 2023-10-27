de394e53e764a88bf7ee1fac2a8db7fbaee7a7cb
Cristian Cocos <cristi@ieee.org>
2023-10-27 17:09:59 -0400

    Use udisksctl instead of mount.

--- Code/Linux/mountUSB.sh
+++ Code/Linux/mountUSB.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# Run as root/sudo.
-if [ ! $(id -u) == 0 ]
+# Do NOT run as root! 
+if [ $(id -u) == 0 ]
 then
-  echo "This script should be run as root! Exiting ..."
+  echo "This script should NOT be run as root! Exiting ..."
   exit
 fi
 
-mkdir -p /run/media/dad/CloneZilla /run/media/dad/InstallationKits /run/media/dad/VentoyCZ /run/media/dad/VentoyIK
-mount /dev/disk/by-uuid/4affd149-c8de-4987-83a7-8281e97f6eba /run/media/dad/CloneZilla
-mount /dev/disk/by-uuid/604729ba-585f-44ca-9794-4c06c22195cc /run/media/dad/InstallationKits
-mount /dev/disk/by-uuid/E1BD-1D00 /run/media/dad/VentoyCZ
-mount /dev/disk/by-uuid/68EF-2812 /run/media/dad/VentoyIK
+# mkdir -p /run/media/dad/CloneZilla /run/media/dad/InstallationKits /run/media/dad/VentoyCZ /run/media/dad/VentoyIK
+udisksctl mount -b /dev/disk/by-uuid/4affd149-c8de-4987-83a7-8281e97f6eba # /run/media/dad/CloneZilla
+udisksctl mount -b /dev/disk/by-uuid/604729ba-585f-44ca-9794-4c06c22195cc # /run/media/dad/InstallationKits
+udisksctl mount -b /dev/disk/by-uuid/E1BD-1D00 # /run/media/dad/VentoyCZ
+udisksctl mount -b /dev/disk/by-uuid/68EF-2812 # /run/media/dad/VentoyIK
