541c132332fbe61b5c4a005ec8238935e62c2884
Cristian Cocos <cristi@ieee.org>
2023-12-01 23:14:47 -0500

    No sudo needed.

--- Code/Linux/BuildScripts/DadsGram.sh
+++ Code/Linux/BuildScripts/DadsGram.sh
@@ -63,6 +63,6 @@ systemctl restart gdm.service
 npm install react react-dom @excalidraw/excalidraw
 
 # Install opentabletdriver.
-# sudo tar -xvf opentabletdriver-0.6.3.0-x64.tar.gz --strip 1
+# tar -xvf opentabletdriver-0.6.3.0-x64.tar.gz --strip 1
 
 # echo "Please power off, and make sure you run UpdateDadsGram.sh!"
