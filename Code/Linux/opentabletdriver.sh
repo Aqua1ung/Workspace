d701331d3da3cb06d8fe5a0d718c4b442fe8f7c7
Cristian Cocos <cristi@ieee.org>
2023-12-04 14:35:52 -0500

    Finished OTD.

--- Code/Linux/opentabletdriver.sh
+++ Code/Linux/opentabletdriver.sh
@@ -5,16 +5,19 @@ sudo -u $user ./dotnet-install.sh --channel 6.0 # Not sudo!
 rm dotnet-install.sh
 
 # Append these two lines to /usr/share/defaults/etc/profile:
+tee -a /usr/share/defaults/etc/profile << EOF
+
 export DOTNET_ROOT=$HOME/.dotnet
 export PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools
+EOF
 
 # Download and install OpenTabletDriver
 cd /run/media/dad/InstallationKits
 OTDv=$(curl -s -L -D - https://github.com/OpenTabletDriver/OpenTabletDriver/releases/latest | grep -n -m 1 "<title>" | sed -n 's/^.*tDriver v//p' | sed -n 's/ · OpenT.*$//p') # Grab the latest version number.
-noOD=$(wget -N wget -N https://github.com/OpenTabletDriver/OpenTabletDriver/releases/download/v$OTDv/opentabletdriver-$OTDv-x64.tar.gz 2>&1 | grep -c "Omitting download") # Download OpenTabletDriver nightly.
+noOD=$(wget -N wget -N https://github.com/OpenTabletDriver/OpenTabletDriver/releases/download/v$OTDv/opentabletdriver-$OTDv-x64.tar.gz 2>&1 | grep -c "Omitting download") # Download OpenTabletDriver.
   if [[ $noOD -eq 0 ]]
   then
-    tar -xvf opentabletdriver-0.6.3.0-x64.tar.gz -C / --strip 1
+    tar -xvf opentabletdriver-$OTDv-x64.tar.gz -C / --strip 1
     cp /home/dad/Git/Workspace/Code/Linux/opentabletdriver.service /usr/local/lib/systemd/user
   else
     echo "No update required."
