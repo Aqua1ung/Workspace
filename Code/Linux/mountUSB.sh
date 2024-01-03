64374cee505d3218885b1b9df2ca7cb931bca8fb
Cristian Cocos <cristi@ieee.org>
2024-01-03 13:24:03 -0500

    UUIDs have changed.

--- Code/Linux/mountUSB.sh
+++ Code/Linux/mountUSB.sh
@@ -8,7 +8,7 @@ then
 fi
 
 # mkdir -p /run/media/dad/CloneZilla /run/media/dad/InstallationKits /run/media/dad/VentoyCZ /run/media/dad/VentoyIK
-udisksctl mount -b /dev/disk/by-uuid/4affd149-c8de-4987-83a7-8281e97f6eba # /run/media/dad/CloneZilla
-udisksctl mount -b /dev/disk/by-uuid/604729ba-585f-44ca-9794-4c06c22195cc # /run/media/dad/InstallationKits
-udisksctl mount -b /dev/disk/by-uuid/E1BD-1D00 # /run/media/dad/VentoyCZ
-udisksctl mount -b /dev/disk/by-uuid/68EF-2812 # /run/media/dad/VentoyIK
+udisksctl mount -b /dev/disk/by-uuid/694290e7-0460-459b-a59c-6ce976a12b7e # /run/media/dad/CloneZilla
+udisksctl mount -b /dev/disk/by-uuid/8c53f34a-75dd-44de-bd1b-2aa7b0ce5a49 # /run/media/dad/InstallationKits
+udisksctl mount -b /dev/disk/by-uuid/C512-23DF # /run/media/dad/Ventoy
+udisksctl mount -b /dev/disk/by-uuid/3a246465-e7de-4894-981a-9da617ad0ff2 # /run/media/dad/Sandbox
