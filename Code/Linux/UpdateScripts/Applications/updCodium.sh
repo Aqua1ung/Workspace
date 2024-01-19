0768a3ee34d1698ef20d03ffa9b1b771661328fa
Cristian Cocos <cristi@ieee.org>
2024-01-18 19:31:49 -0500

    codium -s requires that VSCodium be up and running.

--- Code/Linux/UpdateScripts/Applications/updCodium.sh
+++ Code/Linux/UpdateScripts/Applications/updCodium.sh
@@ -10,8 +10,12 @@ fi
 
 location=$(curl -s -L -D - https://github.com/VSCodium/vscodium/releases/latest/ -o /dev/null -w '%{url_effective}' | grep location | tr -d '\r')
 tag=$(echo "$location" | sed 's/location: https.\+tag\///')
-instVer=$(sudo -u dad codium -s | sed -n 1p | sed -n 's/^.\+VSCodium //p' | sed -n 's/ (.\+$//p' | sed -n 's/ /\./p') # Check installed version.
-if [ "$instVer" != "$tag" ]
+instRel=$(cat /usr/share/codium/resources/app/package.json | grep release | sed -n 's/  "rel.* "//p' |sed -n 's/".*$//p')
+iV=$(sudo -u dad codium -v | sed -n 1p)
+iV+=.$instRel
+# echo "iV  is: $iV"
+# echo "tag is: $tag"
+if [[ "$iV" != "$tag" ]]
 then
   sudo -u dad wget -O /home/dad/Downloads/codium.rpm https://github.com/VSCodium/vscodium/releases/download/$tag/codium-$tag-el7.x86_64.rpm
   rpm -Uvh --nodeps /home/dad/Downloads/codium.rpm
