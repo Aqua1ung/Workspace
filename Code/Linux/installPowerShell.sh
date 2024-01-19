0aabf9e3f521368c2a20255c55bb7d3a0003a23b
Cristian Cocos <cristi@ieee.org>
2024-01-19 18:39:32 -0500

    Only install latest.

--- Code/Linux/installPowerShell.sh
+++ Code/Linux/installPowerShell.sh
@@ -11,9 +11,15 @@ fi
 location=$(curl -s -L -D - https://github.com/PowerShell/PowerShell/releases/latest/ -o /dev/null -w '%{url_effective}' | grep location | tr -d '\r')
 tag=$(echo "$location" | sed 's/location: https.\+tag\///')
 ver=$(echo "$tag" | sed 's/v//') # Replace "v" with nothing.
-sudo -u dad curl -L -o powershell.tar.gz https://github.com/PowerShell/PowerShell/releases/download/$tag/powershell-$ver-linux-x64.tar.gz
-# rm -rf /opt/microsoft/powershell
-mkdir -p /opt/microsoft/powershell
-tar zxf powershell.tar.gz -C /opt/microsoft/powershell
-chmod +x /opt/microsoft/powershell/pwsh
-ln -s -f /opt/microsoft/powershell/pwsh /usr/bin/pwsh
+iV=$(pwsh --version | sed 's/PowerShell //')
+if [[ "$iV" != "$ver" ]]
+then
+  sudo -u dad curl -L -o powershell.tar.gz https://github.com/PowerShell/PowerShell/releases/download/$tag/powershell-$ver-linux-x64.tar.gz
+  # rm -rf /opt/microsoft/powershell
+  mkdir -p /opt/microsoft/powershell
+  tar zxf powershell.tar.gz -C /opt/microsoft/powershell
+  chmod +x /opt/microsoft/powershell/pwsh
+  ln -s -f /opt/microsoft/powershell/pwsh /usr/bin/pwsh
+else
+  echo "No PowerShell update required."
+fi
