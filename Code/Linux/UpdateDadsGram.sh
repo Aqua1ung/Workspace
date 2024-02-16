455e30ce7eb34c508d39b396ee1d96939c529d43
Cristian Cocos <cristi@ieee.org>
2024-02-16 14:12:54 -0500

    Updated Chrome install with Flatpak.

--- Code/Linux/UpdateDadsGram.sh
+++ Code/Linux/UpdateDadsGram.sh
@@ -63,14 +63,33 @@ fi
 printf '\n' # Insert blank line.
 
 # Download and install/update Chrome.
-read -p "Do you want to install/update Chrome? (Y/N) " -n 1 chr
-printf '\n' # Skip to new line.
-if [ $chr == y ] || [ $chr == Y ]
+if [ ! -d /var/lib/flatpak/app/com.google.Chrome ]
 then
-  chmod +x /home/dad/Git/Workspace/Code/Linux/UpdateScripts/Applications/chrome.sh
-  sudo -u dad /home/dad/Git/Workspace/Code/Linux/UpdateScripts/Applications/chrome.sh
+  read -p "Do you want to install or update Chrome? (Y/N) " -n 1 chr
+  printf '\n' # Skip to new line.
+  if [ $chr == y ] || [ $chr == Y ]
+  then
+    read -p "What flavor? (Type 1 for native, anything else for Flatpak.) " -n 1 fon
+    printf '\n' # Insert blank line.
+    if [ $fon -eq 1 ]
+    then # Install or update the native flavor.
+      chmod +x /home/dad/Git/Workspace/Code/Linux/UpdateScripts/Applications/chrome.sh
+      sudo -u dad /home/dad/Git/Workspace/Code/Linux/UpdateScripts/Applications/chrome.sh
+    else # Install the Flatpak flavor.
+      sudo -u dad flatpak install --or-update --noninteractive -y --system com.google.Chrome
+      chmod +x /home/dad/Git/Workspace/Code/Linux/patchFlatseal.sh
+      sudo -u dad /home/dad/Git/Workspace/Code/Linux/patchFlatseal.sh
+      if [ ! -f /home/dad/.local/share/flatpak/overrides/com.google.Chrome ]
+      then
+        echo "WARNING! Flatseal override did not go through! Check Flatseal Chrome permission settings. Exiting script."
+        exit 1
+      fi
+    fi
+  else
+    echo "Skipping Chrome install/update."
+  fi
 else
-  echo "Skipping Chrome install/update."
+  echo "Chrome Flatpak is already installed."
 fi
 printf '\n' # Insert blank line.
 
--- Code/Linux/patchFlatseal.sh
+++ Code/Linux/patchFlatseal.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 # Do NOT run as root!
+if [ $(id -u) == 0 ]
+then
+  echo "This script should NOT be run as root! Exiting ..."
+  exit 1
+fi
 
 # Patch Chrome permissions in FlatSeal for the installation of PWAs.
-flatpak override --user --filesystem=/home/dad/.local/share/applications:create --filesystem=/home/dad/.local/share/icons:create com.google.Chrome
+flatpak override --user --filesystem=~/.local/share/applications --filesystem=~/.local/share/icons com.google.Chrome
