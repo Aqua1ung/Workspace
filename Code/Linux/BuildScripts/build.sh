09aea31ba961e18ac895483a04d108fa8ffd7c0d
Cristian Cocos <cristi@ieee.org>
2024-02-22 13:17:09 -0500

    Added input-remapper and removed FreeRDP.

--- Code/Linux/BuildScripts/build.sh
+++ Code/Linux/BuildScripts/build.sh
@@ -39,7 +39,7 @@ then
 fi
 
 # Install swupd bundles.
-swupd bundle-add lm-sensors firmware-update v4l-utils openssh-server gnome-remote-desktop wine Solaar-gui network-basic xdg-desktop-portal xdg-desktop-portal-gnome x11-tools transcoding-support package-utils java-basic nfs-utils waypipe devpkg-nfs-utils storage-utils python3-basic Remmina nmap nodejs-basic dev-utils-gui audio-pipewire devpkg-libwacom kvm-host hardinfo FreeRDP # containers-basic
+swupd bundle-add lm-sensors firmware-update v4l-utils openssh-server gnome-remote-desktop wine Solaar-gui network-basic xdg-desktop-portal xdg-desktop-portal-gnome x11-tools transcoding-support package-utils java-basic nfs-utils waypipe devpkg-nfs-utils storage-utils python3-basic Remmina nmap nodejs-basic dev-utils-gui audio-pipewire devpkg-libwacom kvm-host hardinfo input-remapper # containers-basic
 
 cd /home/$user/Downloads
 
