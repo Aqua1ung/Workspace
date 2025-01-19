d55e447875a4177221aa35012fe5fab4aa2a26d1
Cristian Cocos <cristi@ieee.org>
2025-01-19 15:43:23 -0500

    Renamed portainer agent.

--- Code/Linux/BuildScripts/DadsGram.sh
+++ Code/Linux/BuildScripts/DadsGram.sh
@@ -124,7 +124,7 @@ sudo cp ~/Git/Workspace/Code/Linux/BuildScripts/fstab /etc
 sudo systemctl daemon-reload
 
 # Install Portainer agent.
-# docker run -d   -p 9001:9001   --name portainer_agent   --restart=always   -v /var/run/docker.sock:/var/run/docker.sock   -v /var/lib/docker/volumes:/var/lib/docker/volumes   -v /:/host   portainer/agent:2.26.0
+# docker run -d   -p 9001:9001   --name PortainerAgentDadsGram   --restart=always   -v /var/run/docker.sock:/var/run/docker.sock   -v /var/lib/docker/volumes:/var/lib/docker/volumes   -v /:/host   portainer/agent:2.26.0
 
 # Configure jEnv.
 # echo 'export PATH="$HOME/.jenv/bin:$PATH"' >> ~/.bashrc
